8a46e53fad35649b2b6640040e03771d7d46c827
Michael Graff <explorer@flame.org>
2021-01-03 11:20:23 -0600

    refactor

--- main.go
+++ main.go
@@ -10,7 +10,16 @@ import (
 	"time"
 )
 
-func main() {
+var (
+	config *appConfig
+)
+
+type appConfig struct {
+	targetname string
+	namespace  string
+}
+
+func makeConfig() *appConfig {
 	env := os.Environ()
 	for _, item := range env {
 		log.Printf("Environment: %s", item)
@@ -24,14 +33,28 @@ func main() {
 	targetname := fmt.Sprintf("grpc-cluster.%s.svc.cluster.local", namespace)
 	log.Printf("Using service discovery hostname: %s", targetname)
 
+	return &appConfig{
+		targetname: targetname,
+		namespace:  namespace,
+	}
+}
+
+func getPeerAddresses() []net.IP {
+	ips, err := net.LookupIP(config.targetname)
+	if err != nil {
+		log.Fatalf("Cannot look up %s: %v", config.targetname, err)
+	}
+	sort.Slice(ips, func(i int, j int) bool {
+		return bytes.Compare(ips[i], ips[j]) < 0
+	})
+	return ips
+}
+
+func main() {
+	config = makeConfig()
+
 	for {
-		ips, err := net.LookupIP(targetname)
-		if err != nil {
-			log.Fatalf("Cannot look up %s: %v", targetname, err)
-		}
-		sort.Slice(ips, func(i int, j int) bool {
-			return bytes.Compare(ips[i], ips[j]) < 0
-		})
+		ips := getPeerAddresses()
 		log.Printf("Found IPs: %v", ips)
 		time.Sleep(5 * time.Second)
 	}
